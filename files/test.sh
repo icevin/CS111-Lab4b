f36ab09726617177f8a0da8cd16d23fb8d968a94
Kevin Tang <kevintang2023@gmail.com>
2020-11-29 23:47:57 -0800

    Quick n dirty smoketest

--- files/test.sh
+++ files/test.sh
@@ -1,1 +1,29 @@
 #!/bin/bash
+
+# NAME: Kevin Tang
+# EMAIL: kevintang2023@ucla.edu
+# ID: 805419480
+
+counter = 0
+
+./lab4b --BOGUS &> /dev/null
+if [[ $? =ne 1]]
+then
+    echo "FAIL: bogus argument test"
+else
+    echo "PASS: bogus argument"
+    ((counter=counter+1))
+fi
+
+{ sleep 2; echo "SCALE=C"; sleep 2; echo "PERIOD=2"; sleep 1; echo "OFF"; } | ./lab4b --log=log.test
+
+if [[ $? -ne 0 ]]
+then
+	echo "FAIL: Program exited unsuccessfully"
+else
+	echo "PASS: Ran successfully!"
+fi
+
+echo "PASSED ${counter} OUT OF 2 CASES"
+
+rm -f log.test
