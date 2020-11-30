37c8d66a3135d70d98c479711023afa2d5a816c0
Kevin Tang <kevintang2023@gmail.com>
2020-11-29 23:55:24 -0800

    Done

--- files/test.sh
+++ files/test.sh
@@ -4,24 +4,27 @@
 # EMAIL: kevintang2023@ucla.edu
 # ID: 805419480
 
-counter = 0
+counter=0
 
 ./lab4b --BOGUS &> /dev/null
-if [[ $? =ne 1]]
+if [[ $? -ne 1 ]]
 then
-    echo "FAIL: bogus argument test"
+	echo "FAIL: bogus argument test"
 else
-    echo "PASS: bogus argument"
-    ((counter=counter+1))
+	echo "PASS: bogus argument"
+	((counter=counter+1))
 fi
 
-{ sleep 2; echo "SCALE=C"; sleep 2; echo "PERIOD=2"; sleep 1; echo "OFF"; } | ./lab4b --log=log.test
+echo "Testing program execution..."
+
+{ sleep 2; echo "SCALE=C"; sleep 2; echo "PERIOD=2"; sleep 1; echo "OFF"; } | ./lab4b --log=log.test > /dev/null
 
 if [[ $? -ne 0 ]]
 then
 	echo "FAIL: Program exited unsuccessfully"
 else
 	echo "PASS: Ran successfully!"
+	((counter=counter+1))
 fi
 
 echo "PASSED ${counter} OUT OF 2 CASES"
